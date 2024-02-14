1eb329c558b5c43e62aa45c29b12cf19698a7785
Miki Stec <mikulas@stec.sk>
2024-02-14 20:19:00 +0800

    Theme colors

---
+++ src/app/theme/page.tsx
@@ -0,0 +1,108 @@
+export default function Page() {
+  return (
+    <>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-primary/15 w-10 h-10 mr-2"></div>
+        <span>Primary/15</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-background w-10 h-10 mr-2"></div>
+        <span>background</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-foreground w-10 h-10 mr-2"></div>
+        <span>foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-card w-10 h-10 mr-2"></div>
+        <span>card</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-card-foreground w-10 h-10 mr-2"></div>
+        <span>card-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-popover w-10 h-10 mr-2"></div>
+        <span>popover</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-popover-foreground w-10 h-10 mr-2"></div>
+        <span>popover-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-primary w-10 h-10 mr-2"></div>
+        <span>primary</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-primary-foreground w-10 h-10 mr-2"></div>
+        <span>primary-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-secondary w-10 h-10 mr-2"></div>
+        <span>secondary</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-secondary-foreground w-10 h-10 mr-2"></div>
+        <span>secondary-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-muted w-10 h-10 mr-2"></div>
+        <span>muted</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-muted-foreground w-10 h-10 mr-2"></div>
+        <span>muted-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-accent w-10 h-10 mr-2"></div>
+        <span>accent</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-accent-foreground w-10 h-10 mr-2"></div>
+        <span>accent-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-destructive w-10 h-10 mr-2"></div>
+        <span>destructive</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-destructive-foreground w-10 h-10 mr-2"></div>
+        <span>destructive-foreground</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-border w-10 h-10 mr-2"></div>
+        <span>border</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-input w-10 h-10 mr-2"></div>
+        <span>input</span>
+      </div>
+
+      <div className="flex items-center mb-2">
+        <div className="bg-ring w-10 h-10 mr-2"></div>
+        <span>ring</span>
+      </div>
+
+
+    </>
+  );
+}
