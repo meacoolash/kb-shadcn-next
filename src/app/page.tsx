c1812ca91a9b2754bea4f7e1c87865b6679cb73d
Miki Stec <mikulas@stec.sk>
2024-02-14 20:16:10 +0800

    Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex min-h-screen flex-col ">
+
+      <Button className="w-96 m-3">Click me</Button>
+
     </main>
   );
 }
